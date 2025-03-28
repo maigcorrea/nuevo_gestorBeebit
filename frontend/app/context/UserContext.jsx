cd11fab38efeb20e94c24e0c554c38d4b9acb230
Maite <anamaitegarciacorrea@gmail.com>
2025-03-28 10:24:18 +0100

    Arreglo el context, al recargar la página se reiniciaba toda la app, incluido el context, que guardaba el localStorage y se ponía a bull, si no se leía de nuevo el localStorage y se volvía a guardar, ya no existía ese dato

--- frontend/app/context/UserContext.jsx
+++ frontend/app/context/UserContext.jsx
@@ -1,19 +1,34 @@
 'use client';
 import { createContext, useState, useEffect } from 'react';
 
-export const UserContext = createContext(); //Var que se exporta
+// Un contexto en React es como una "memoria compartida" entre componentes.
+// En este caso, el contexto guarda el tipo de usuario (userType) y tiene una función setUserType para cambiarlo.
 
+//Creamos el contexto con un valor por defecto
+export const UserContext = createContext({
+  userType:null,
+  setUserType:() => {} // Función vacía para evitar errores antes de que se defina de verdad (en el Provider)
+}); //Var que se exporta
+
+
+// Creamos el Provider, que será el encargado de guardar el estado global del usuario.
 export const UserProvider = ({ children }) => {
-    //Un contexto en React es como una "memoria compartida" entre componentes.
-    //En este caso, el contexto guarda el tipo de usuario (userType) y tiene una función setUserType para cambiarlo.
-    const [userType, setUserType] = useState(null); //Obtener el tipo de usuario del localStorage
+    // Inicializamos el estado. De primeras es null, hasta que comprobemos si hay algo guardado en localStorage.
+    const [userType, setUserType] = useState(null); //Obtener el tipo de usuario del localStorage, si no hay nada, poner a null
 
+  //Sólo se ejecuta una vez al cargar la página
   useEffect(() => {
-    const type = localStorage.getItem('userType');
-    setUserType(type);
+    // Busca el tipo de usuario en el localStorage (Se guarda ahí tras iniciar sesión en el LoginForm)
+    const storedType = localStorage.getItem('type');
+
+    // Si lo encuentra, lo actualiza en el estado 
+    if (storedType) {
+      setUserType(storedType);
+    }
   }, []);
 
   return (
+    // Envolvemos a toda la app (o una parte de ella) en un Provider que comparte ese estado a todos los componentes hijos. 
     <UserContext.Provider value={{ userType, setUserType }}>
       {children}
     </UserContext.Provider>
